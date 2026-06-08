5791eaca388723a3db006e364e4a69bca15fcfb8
Naga Aditya Anand Chintalapudi <141710395+Naga-Aditya-Anand@users.noreply.github.com>
2026-06-08 13:53:05 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from tools.yfinance_tools import get_company_profile, get_balance_sheet
-
-print("hello")
-
-print(get_company_profile.invoke({"ticker": "APOLLO"}))
-print(get_balance_sheet.invoke({"ticker": "APOLLO"}))
-# print(search_stock_news.invoke({"ticker": "INFY"}))
-# print(search_india_macro_news.invoke({}))
